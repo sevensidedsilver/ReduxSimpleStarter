07d4b3b4d9e909f08e1e8ebb915d4949af0d8720
Sean <sevensidedsilver@gmail.com>
2017-07-19 10:57:41 -0600

    working

--- src/index.js
+++ src/index.js
@@ -44,7 +44,7 @@ class App extends Component {
   }
 
   render(){
-    const videoSearch = _.debounce((term)=>{this.videoSearch(term) }, 400)
+    const videoSearch = _.debounce((term)=>{this.videoSearch(term) }, 200)
 
     return (
       <div>
